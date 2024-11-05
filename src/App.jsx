3919ee2dfd6a24d47377158d4ee0e58456fd40c3
Dhinesh <dhineshkbd3@email>
2024-11-05 15:13:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Stopwatch from './Stopwatch'
+
+function App() {
+  
+  return (
+   <>
+    <Stopwatch/>
+    </>
+  )
+}
+
+export default App
